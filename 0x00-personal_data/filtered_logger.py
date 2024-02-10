02870b700b2d576ad3c7b0ba5930ed70172f6b20
Domodan <ongomdaniel9@gmail.com>
2024-02-10 17:20:55 +0000

    updated the class to accept a list of strings fields constructor argument

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,10 +69,11 @@ def main():
     cursor.close()
     db.close()
 
-
+'''
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """
+        Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -83,11 +84,13 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Filters values in incoming log records using filter_datum """
+        """
+            Filters values in incoming log records using filter_datum
+        """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
-
+'''
 
 if __name__ == '__main__':
-    main()'''
+    main()
