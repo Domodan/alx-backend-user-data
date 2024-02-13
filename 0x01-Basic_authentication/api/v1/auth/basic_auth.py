39771b36613a170fdda0cd83496dd458a25a7d71
Domodan <ongomdaniel9@gmail.com>
2024-02-13 10:05:12 +0000

    added the method def current_user(self, request=None) -> TypeVar('User') in the class BasicAuth that overloads Auth and retrieves the User instance for a request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -94,9 +94,9 @@ class BasicAuth(Auth):
         """
         try:
             auth_header = self.authorization_header(request)
-            b64_auth_token = self.extract_base64_authorization_header(auth_header)
-            auth_token = self.decode_base64_authorization_header(b64_auth_token)
-            email, password = self.extract_user_credentials(auth_token)
+            auth_token = self.extract_base64_authorization_header(auth_header)
+            token = self.decode_base64_authorization_header(auth_token)
+            email, password = self.extract_user_credentials(token)
             return self.user_object_from_credentials(email, password)
         except Exception:
             return None
