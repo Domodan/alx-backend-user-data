22fa530e965fcdebc902bd3dedd97699d19432fe
Domodan <ongomdaniel9@gmail.com>
2024-02-13 10:08:03 +0000

    improved the method def extract_user_credentials(self, decoded_base64_authorization_header) to allow password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,7 @@ class BasicAuth(Auth):
             Extracts user credentials from a base64-decoded authorization
         """
         if type(decoded_base64_authorization_header) == str:
-            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            pattern = r'(?P<user>[^]+)(?P<password>.+)'
             field_match = re.fullmatch(
                 pattern,
                 decoded_base64_authorization_header.strip(),
