5c4100635a3d430b13ce6bab70743c2a741d1bb0
Domodan <ongomdaniel9@gmail.com>
2024-02-22 11:01:54 +0000

    implemented the Auth.create_session method that takes an email string argument and returns the session ID as a string

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,7 +104,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    '''def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+    '''def get_user_from_session_id(self, session_id: str) -> Union[User, No]:
         """Retrieves a user based on a given session ID.
         """
         user = None
